0da1e280f6de22dba3f963bcab3147eaa210b5e9
salmansohail20 <salmansohail20@gmail.com>
2025-11-22 12:23:44 +0500

    navbar responsive

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -1,31 +1,33 @@
 import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
 import "./Navbar.scss";
 
 const Navbar = () => {
   const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <div className="head_nav_wrap">
+    <div className={`head_nav_wrap ${isMenuOpen ? 'show' : ''}`}>
       <div className="container ">
         <div className="navbar_brand">
           <Link to="/">
             <img src="/images/logo.svg" alt="" />
           </Link>
         </div>
-        <button className="burger_btn"><i className="ico ico-burger-list-menu-navigation-svgrepo-com"></i></button>
-        <nav className="head_navbar">
+        <button className="burger_btn" onClick={() => setIsMenuOpen(!isMenuOpen)}><i className="ico ico-burger-list-menu-navigation-svgrepo-com"></i></button>
+        <nav className={`head_navbar ${isMenuOpen ? 'show' : ''}`}>
           <ul className="navbar_links">
-            <li className={location.pathname === "/analytics" ? "active" : ""}>
+            <li className={location.pathname === "/analytics" ? "active" : ""} onClick={() => setIsMenuOpen(!isMenuOpen)}>
               <Link to="/analytics">Analytics</Link>
             </li>
-            <li className={location.pathname === "/compare" ? "active" : ""}>
+            <li className={location.pathname === "/compare" ? "active" : ""} onClick={() => setIsMenuOpen(!isMenuOpen)}>
               <Link to="/compare">Compare</Link>
             </li>
             <li
-              className={location.pathname === "/competitors" ? "active" : ""}
+              className={location.pathname === "/competitors" ? "active" : ""} onClick={() => setIsMenuOpen(!isMenuOpen)}
             >
               <Link to="/competitors">Competitors</Link>
             </li>
-            <li className={location.pathname === "/settings" ? "active" : ""}>
+            <li className={location.pathname === "/settings" ? "active" : ""} onClick={() => setIsMenuOpen(!isMenuOpen)}>
               <Link to="/settings">Settings</Link>
             </li>
           </ul>
